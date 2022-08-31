75db2d5b34be0ddb4403fdc4f46b7a546914935c
Aomsinnumber10 <112546001+Aomsinnumber10@users.noreply.github.com>
2022-08-31 15:40:12 +0700

    Add files via upload

---
+++ Aomsinj.py
@@ -0,0 +1,22 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================================== RESTART: C:/Users/Praharuthai Convent/pink.py ===================================
+Traceback (most recent call last):
+  File "C:/Users/Praharuthai Convent/pink.py", line 1, in <module>
+    Input("What is your name?")
+NameError: name 'Input' is not defined. Did you mean: 'input'?
+
+=================================== RESTART: C:/Users/Praharuthai Convent/pink.py ===================================
+What is your name?
+=================================== RESTART: C:/Users/Praharuthai Convent/pink.py ===================================
+What is your name? Pink
+Hello
+
+=================================== RESTART: C:/Users/Praharuthai Convent/pink.py ===================================
+What is your name? Pink
+Hello, Pink
+
+=================================== RESTART: C:/Users/Praharuthai Convent/pink.py ===================================
+What is your name? Aomsin
+Hello, Aomsin
